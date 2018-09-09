f311538bfdf56e7703469ce1f7cdacf65ef83ff4
Benjamin Troller <bctroller@gmail.com>
2018-09-08 20:59:56 -0700

    I've finished farming

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,115 @@
+use std::io;
+
+struct Player {
+    name: String,
+    scores: Vec<i32>,
+    totalScore: i32,
+}
+
+// Assign a player an additional score of `score` for a hole
+impl Player {
+    fn justScored(&mut self, score: i32) {
+        self.scores.push(score);
+        self.totalScore += score;
+    }
+}
+
+/*
+impl Player {
+    fn totalScore(self) -> i32 {
+        let sumOfScores = self.scores.iter().sum();
+        return sumOfScores;
+    }
+}
+*/
+
+fn scanString() -> String {
+    loop {
+        let mut scannedLine = String::new();
+
+        io::stdin().read_line(&mut scannedLine)
+            .expect("Failed to read line");
+
+        return scannedLine.trim().to_string();
+    }
+}
+
+fn scanInt() -> i32 {
+    loop {
+        let scannedString = scanString();
+
+        match scannedString.parse() {
+            Ok(num) => return num,
+            Err(_) => continue,
+        };
+    }
+}
+
+fn scanStrictlyPositiveInt() -> i32 {
+    loop {
+        let scannedInt = scanInt();
+        if scannedInt > 0 {
+            return scannedInt;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Get num holes to play
+    println!("How many holes will you play?");
+    let numHolesToPlay = scanStrictlyPositiveInt();
+    let mut numHolesPlayed: i32 = 0;
+
+    // Get num players who will play
+    println!("How many players?");
+    let numPlayers = scanStrictlyPositiveInt();
+
+    // Make Vec of Players
+    let mut players: Vec<Player> = Vec::new();
+    for playerNum in (1..(numPlayers + 1)) {
+        // Prompt each player for name
+        println!("What is player {}'s name?", playerNum);
+        let scannedPlayerName = scanString();
+        let mut player = Player { scores: vec![0], name: scannedPlayerName, totalScore: 0 };
+        // Put player in `players` Vec
+        players.push(player);
+    }
+
+    // Loop for each hole of the game
+    while numHolesPlayed < numHolesToPlay {
+        numHolesPlayed += 1;
+
+        // Print current standings
+        println!();
+        println!("Hole {} standings: ", numHolesPlayed);
+        players.sort_by(|a, b| b.totalScore.cmp(&a.totalScore)); 
+        for player in players.iter() {
+            print!("  {} has {}:  ", player.name, player.totalScore);
+            for individualHoleScore in player.scores.iter() {
+                print!("{} ", individualHoleScore);
+            }
+            println!();
+        }
+
+        // Ask for results of hole
+        println!();
+        for mut player in players.iter_mut() {
+            println!("What did {} score?", player.name);
+            let score = scanInt();
+            player.justScored(score);
+        }
+    }
+
+    // Print final standings
+    println!();
+    println!("Final standings:");
+    players.sort_by(|a, b| b.totalScore.cmp(&a.totalScore)); 
+    for player in players.iter() {
+        print!("  {} has {}:  ", player.name, player.totalScore);
+        for individualHoleScore in player.scores.iter() {
+            print!("{} ", individualHoleScore);
+        }
+        println!();
+    }
+
 }
